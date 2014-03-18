3c430fc9bc843a5e701364325764a865c142fa1b
Mikola Lysenko <mikolalysenko@gmail.com>
2014-03-17 19:06:54 -0500

    fixed bug with axes locations

--- lib/shaders/textVert.glsl
+++ lib/shaders/textVert.glsl
@@ -5,6 +5,7 @@ attribute vec2 screenOffset;
 
 uniform mat4 model, view, projection;
 uniform vec3 offset, axis;
+uniform float textScale;
 
 varying float fragTexId;
 varying vec2 fragTexCoord;
@@ -12,7 +13,7 @@ varying vec2 fragTexCoord;
 void main() {
   vec4 worldPosition = model * vec4(t * axis + offset, 1);
   vec4 viewPosition = view * worldPosition;
-  gl_Position = projection * (viewPosition + 0.25 * vec4(screenOffset, 0, 1));
+  gl_Position = projection * (viewPosition + textScale * vec4(screenOffset, 0, 0));
   fragTexId = textureId;
   fragTexCoord = texCoord;
 }
